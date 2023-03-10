f767ded038263ccaedf20241edf8c5b136c1fdfd
Egor <egor@fasthome.io>
2023-03-10 16:26:19 +0300

    avatar

--- src/pages/main/nested-pages/company/nested-pages/messages/index.tsx
+++ src/pages/main/nested-pages/company/nested-pages/messages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import styles from './styles.module.scss';
+import { Avatar } from '../../../../../../shared/ui';
 
 function MessagesPage() {
     return <div className={styles.page}>MessagesPage</div>;
