3930dd68762b0a20055fcd3507f8b00ddcf6ce19
Egor <egorraybul@gmail.com>
2023-05-31 11:43:48 +0300

    upd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,62 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayEvent};
+use tauri::Manager;
+
 fn main() {
-  tauri::Builder::default()
- .on_window_event(|event| match event.event() {
+
+  let quit = CustomMenuItem::new("quit".to_string(), "Закрыть");
+  let hide = CustomMenuItem::new("hide".to_string(), "Скрыть");
+  let tray_menu = SystemTrayMenu::new()
+    .add_item(quit)
+    .add_item(hide);
+
+  let system_tray = SystemTray::new()
+      .with_menu(tray_menu);
+
+  tauri::Builder::default().system_tray(system_tray)
+
+     .on_system_tray_event(|app, event| match event {
+       SystemTrayEvent::LeftClick {
+         position: _,
+         size: _,
+         ..
+       } => {
+         println!("system tray received a left click");
+          let window = app.get_window("main").unwrap();
+                      window.show().unwrap();
+       }
+       SystemTrayEvent::RightClick {
+         position: _,
+         size: _,
+         ..
+       } => {
+         println!("system tray received a right click");
+       }
+       SystemTrayEvent::DoubleClick {
+         position: _,
+         size: _,
+         ..
+       } => {
+         println!("system tray received a double click");
+       }
+       SystemTrayEvent::MenuItemClick { id, .. } => {
+         match id.as_str() {
+           "quit" => {
+             std::process::exit(0);
+           }
+           "hide" => {
+             let window = app.get_window("main").unwrap();
+             window.hide().unwrap();
+           }
+           _ => {}
+         }
+       }
+       _ => {}
+     })
+
+  .on_window_event(|event| match event.event() {
    tauri::WindowEvent::CloseRequested { api, .. } => {
      event.window().hide().unwrap();
      api.prevent_close();
