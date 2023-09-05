6f3caca99ab664bd0c5f27b02fbe382b5a70eb13
Egor <egorraybul@gmail.com>
2023-09-06 01:29:40 +0300

    upd

--- src/entities/contact/ui/profile/index.tsx
+++ src/entities/contact/ui/profile/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { UserCardView } from 'entities/user';
 import { BaseTypes } from 'shared/types';
-import { Avatar, Button, Dropdown, DropdownTypes, Icons, IconsTypes, Title } from 'shared/ui';
 
 import styles from './styles.module.scss';
 import { ContactProxy } from '../../model/types';
@@ -16,24 +15,20 @@ type Props = {
 function ContactProfileView(props: Props) {
     const { contact, back, updateName } = props;
 
-    const secondaryInfo: { id: number; title: string; subtitle: string }[] = [
-        { id: 0, title: 'Никнейм', subtitle: '' },
-        { id: 1, title: 'Номер телефона', subtitle: contact?.phone || '' },
-    ];
-
-    const menuItems: DropdownTypes.DropdownMenuItem[] = [
-        {
-            id: 0,
-            title: '',
-            icon: <Icons variant="delete" />,
-            callback: () => '',
-        },
-    ];
-
     return (
         <div className={styles.wrapper}>
             <div className={styles.card}>
-                <UserCardView visibleActionsMenu name={contact?.full_name || ''} aboutMe="" avatar="" birth="" phone={contact?.phone || ''} nickname="" />
+                <UserCardView
+                    visibleHeader
+                    type="contact"
+                    visibleActionsMenu
+                    name={contact?.full_name || ''}
+                    aboutMe=""
+                    avatar=""
+                    birth=""
+                    phone={contact?.phone || ''}
+                    nickname=""
+                />
             </div>
         </div>
     );
--- src/entities/user/ui/card/index.tsx
+++ src/entities/user/ui/card/index.tsx
@@ -14,10 +14,13 @@ type Props = {
     birth: string;
     aboutMe: string;
     visibleActionsMenu?: boolean;
+    visibleHeader?: boolean;
+    networkStatus?: string;
+    type?: 'contact' | 'employee';
 } & BaseTypes.Statuses;
 
 function UserCardView(props: Props) {
-    const { avatar, name, nickname, aboutMe, birth, phone, loading, visibleActionsMenu } = props;
+    const { networkStatus, visibleHeader, type, avatar, name, nickname, aboutMe, birth, phone, loading, visibleActionsMenu } = props;
 
     const secondaryInfo: { id: number; title: string; subtitle: string }[] = [
         { id: 0, title: 'Никнейм', subtitle: `@${nickname}` },
@@ -26,45 +29,70 @@ function UserCardView(props: Props) {
         { id: 3, title: 'О себе', subtitle: '' },
     ];
 
-    const btns: BaseTypes.Item[] = [
+    const sharedBtn: BaseTypes.Item[] = [
         { id: 0, title: 'Аудио', icon: 'phone', payload: '', callback: () => '' },
         { id: 1, title: 'Видео', icon: 'videocam', payload: '', callback: () => '' },
-        { id: 2, title: 'Ещё', icon: 'more', payload: '', callback: () => '' },
-        { id: 3, title: 'Выкл.', icon: 'mute', payload: '', callback: () => '' },
+        { id: 2, title: 'Чат', icon: 'chat', payload: '', callback: () => '' },
     ];
+
+    const btnsContact: BaseTypes.Item[] = [...sharedBtn, { id: 2, title: 'Ещё', icon: 'more', payload: '', callback: () => '' }];
+    const btnsEmployee: BaseTypes.Item[] = [...sharedBtn, { id: 3, title: 'Выкл.', icon: 'mute', payload: '', callback: () => '' }];
 
     const sm = useWidthMediaQuery().to('sm');
 
     return (
         <Box.Animated visible loading={loading} className={styles.wrapper}>
-            <div className={styles.avatar} style={{ width: sm ? 346 : 375 }}>
-                <Avatar circle={false} size={sm ? 346 : 375} img={avatar} />
-                {visibleActionsMenu && (
-                    <div className={styles.btns} style={{ width: sm ? 346 : 375 }}>
-                        {btns.map((i) => (
-                            <Button
-                                direction="vertical"
-                                prefixIcon={i.id === 3 ? <Icons.Player variant={i.icon} /> : <Icons variant={i.icon} />}
-                                onClick={i.callback}
-                            >
-                                {i.title}
-                            </Button>
+            {visibleHeader && (
+                <div className={styles.header}>
+                    <div className={styles.name}>
+                        <Title variant="H1">{name}</Title>
+                        <Button tag>dw</Button>
+                    </div>
+                    <Title textAlign="right" variant="H4M">
+                        {networkStatus}
+                    </Title>
+                </div>
+            )}
+            <div className={styles.body}>
+                <div className={styles.avatar} style={{ width: sm ? 346 : 375 }}>
+                    <Avatar circle={false} size={sm ? 346 : 375} img={avatar} />
+                    {visibleActionsMenu && (
+                        <div className={styles.btns} style={{ width: sm ? 346 : 375 }}>
+                            {type === 'contact'
+                                ? btnsContact.map((i) => (
+                                      <Button
+                                          direction="vertical"
+                                          prefixIcon={i.id === 3 ? <Icons.Player variant={i.icon} /> : <Icons variant={i.icon} />}
+                                          onClick={i.callback}
+                                      >
+                                          {i.title}
+                                      </Button>
+                                  ))
+                                : btnsEmployee.map((i) => (
+                                      <Button
+                                          direction="vertical"
+                                          prefixIcon={i.id === 3 ? <Icons.Player variant={i.icon} /> : <Icons variant={i.icon} />}
+                                          onClick={i.callback}
+                                      >
+                                          {i.title}
+                                      </Button>
+                                  ))}
+                        </div>
+                    )}
+                </div>
+
+                <div className={styles.info}>
+                    {!visibleHeader && <Title variant="H1">{name}</Title>}
+                    <div className={styles.secondaryInfo}>
+                        {secondaryInfo.map((i) => (
+                            <div key={i.id} className={styles.item}>
+                                <Title variant="H4M" primary={false}>
+                                    {i.title}
+                                </Title>
+                                <Title variant="H3M">{i.subtitle}</Title>
+                            </div>
                         ))}
                     </div>
-                )}
-            </div>
-
-            <div className={styles.info}>
-                <Title variant="H1">{name}</Title>
-                <div className={styles.secondaryInfo}>
-                    {secondaryInfo.map((i) => (
-                        <div key={i.id} className={styles.item}>
-                            <Title variant="H4M" primary={false}>
-                                {i.title}
-                            </Title>
-                            <Title variant="H3M">{i.subtitle}</Title>
-                        </div>
-                    ))}
                 </div>
             </div>
         </Box.Animated>
