b0849fd2f520ab61fb4b42bc766228e2d8494d57
Egor <egorraybul@gmail.com>
2023-12-22 22:13:43 +0300

    upd

--- web-py-scripts/scripts/build_and_send.py
+++ web-py-scripts/scripts/build_and_send.py
@@ -1,22 +1,41 @@
-from bin.dir_workers import delete_dir
 import os, requests
 from pathlib import Path
 import platform
 
+import os, stat, shutil
+
+
+def remove_readonly(fn, path, _):
+    try:
+        os.chmod(path, stat.S_IWRITE)
+        fn(path)
+    except Exception as exc:
+        print("Skipped:", path, "because:\n", exc)
+
+
+def delete_dir(dir_name):
+    check_dir = os.path.isdir(dir_name)
+    print(check_dir)
+    if check_dir:
+        shutil.rmtree(dir_name, onerror=remove_readonly)
+
+
 if __name__ == '__main__':
     platform = platform.system()
     server_endpoint_name = "http://localhost:5000/api/v1/files/upload_desktop_release"
-        # "https://dev.api.confee.ru/api/v2/release"
+    # "https://dev.api.confee.ru/api/v2/release"
 
     project_dir = Path(__file__).parents[2]
     tauri_dir = Path(project_dir, "src-tauri")
+
+    delete_dir(Path(tauri_dir, "target"))
+
+    os.system("npm run tauri:local-build")
 
     version = ''
     signature = ''
     app = ''
     app_os = ''
-
-    # delete_dir(Path(tauri_dir, "target"))
 
     if platform == 'Windows':
         path = Path(project_dir, "src-tauri", "target", "release", "bundle", "msi")
@@ -35,14 +54,12 @@ if __name__ == '__main__':
             print('no direct')
             exit()
 
-
-
     try:
         data = {"version": version, "signature": signature, "os": app_os}
         res = requests.post(server_endpoint_name, data=data, files=[app],
                             verify=False)
         print(res)
-
-        # delete_dir(Path(tauri_dir, "target"))
+        print('success')
+        delete_dir(Path(tauri_dir, "target"))
     except:
         print('send error')
