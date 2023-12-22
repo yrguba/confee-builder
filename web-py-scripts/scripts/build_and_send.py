9c68d7a6eec3b38942713b5f979d09b9e00d757d
Egor <egorraybul@gmail.com>
2023-12-23 00:11:04 +0300

    upd

--- web-py-scripts/scripts/build_and_send.py
+++ web-py-scripts/scripts/build_and_send.py
@@ -22,40 +22,49 @@ def delete_dir(dir_name):
 
 if __name__ == '__main__':
     platform = platform.system()
-    server_endpoint_name = "http://localhost:5000/api/v1/files/upload_desktop_release"
-    # "https://dev.api.confee.ru/api/v2/release"
+    server_endpoint_name = "https://dev.api.confee.ru/api/v2/release"
 
     project_dir = Path(__file__).parents[2]
     tauri_dir = Path(project_dir, "src-tauri")
 
     project_json = json.load(open(Path(project_dir, 'package.json'), 'r'))
-    print(project_json)
 
-    # delete_dir(Path(tauri_dir, "target"))
-    #
-    # os.system("npm run tauri:local-build")
+    delete_dir(Path(tauri_dir, "target"))
+
+    os.system("npm run tauri:local-build")
 
     version = project_json["version"]
     signature = ''
     app = ''
     app_os = ''
-    
+
     if platform == 'Windows':
         path = Path(project_dir, "src-tauri", "target", "release", "bundle", "msi")
         if os.path.isdir(path):
             files = os.listdir(path)
             app_os = 'windows'
-
             for file in files:
                 if file.split('.').pop() == 'sig':
                     signature = open(Path(path, file), 'r').read()
-
                 if file.split('.').pop() == 'zip':
                     app = ('file', open(Path(path, file), 'rb'))
         else:
             print('no direct')
             exit()
 
+    if platform == 'Darwin':
+        path = Path(project_dir, "src-tauri", "target", "release", "bundle", "macos")
+        if os.path.isdir(path):
+            files = os.listdir(path)
+            app_os = 'mac'
+            for file in files:
+                if file.split('.').pop() == 'sig':
+                    signature = open(Path(path, file), 'r').read()
+                if file.split('.').pop() == 'gz':
+                    app = ('file', open(Path(path, file), 'rb'))
+        else:
+            print('no direct')
+            exit()
     try:
         data = {"version": version, "signature": signature, "os": app_os}
         res = requests.post(server_endpoint_name, data=data, files=[app],
