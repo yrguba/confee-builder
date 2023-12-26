74dc079913f750ef1f8163fe81e55c5e4685700d
Egor <egorraybul@gmail.com>
2023-12-26 13:19:46 +0300

    upd

--- src/pages/main/layouts/ui/index.tsx
+++ src/pages/main/layouts/ui/index.tsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import styles from './styles.module.scss';
+import { appService } from '../../../../entities/app';
 import { chatApi, chatGateway, useChatStore } from '../../../../entities/chat';
 import { meetGateway, useMeetStore } from '../../../../entities/meet';
 import { messageGateway } from '../../../../entities/message';
 import { userGateway } from '../../../../entities/user';
 import { JoinMeetModal } from '../../../../features/meet';
-import { useRouter, useWebSocket } from '../../../../shared/hooks';
+import { useRouter, useWebSocket, useWebView } from '../../../../shared/hooks';
 import useRecognizeSpeech from '../../../../shared/hooks/useRecognizeSpeech';
 import { Modal, Notification } from '../../../../shared/ui';
 import Navbar from '../widgets/navbar';
@@ -24,11 +25,23 @@ function MainLayout() {
     messageGateway();
     meetGateway();
 
+    const webView = useWebView({
+        id: 'join_meet',
+        title: `приглашение в конференцию`,
+        onClose: () => {
+            console.log('close');
+        },
+    });
+
     const joinMeetModal = Modal.use();
 
     useEffect(() => {
         if (joinRequest.value.id) {
-            joinMeetModal.open();
+            if (appService.tauriIsRunning) {
+                webView?.open(`/join_meet`);
+            } else {
+                joinMeetModal.open();
+            }
         }
     }, [joinRequest.value]);
 
