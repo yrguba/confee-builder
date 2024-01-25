32ffa570e06e08e828ff1cb14fe824f8ab61d3ad
Egor <egorraybul@gmail.com>
2024-01-25 03:05:42 +0300

    upd

--- src/entities/message/ui/list/index.tsx
+++ src/entities/message/ui/list/index.tsx
@@ -1,6 +1,5 @@
-import React, { useRef, Fragment, useEffect, useState, RefObject } from 'react';
+import React, { useRef, Fragment, useEffect, useState, RefObject, WheelEvent } from 'react';
 import { mergeRefs } from 'react-merge-refs';
-import { Scrollbar } from 'react-scrollbars-custom';
 import { useLifecycles, useUpdateEffect } from 'react-use';
 
 import { useInView, usePrevious, useScroll, UseStoreTypes, useDimensionsObserver, useEasyState, UseEasyStateReturnType } from 'shared/hooks';
@@ -103,12 +102,15 @@ function MessagesListView(props: Props) {
         }
     };
 
+    const onWheel = (e: WheelEvent<HTMLDivElement>) => {
+        const isScrollUp = e.deltaY < 0;
+        const step = 50;
+        const currentY = wrapperRef.current.scrollTop;
+        wrapperRef.current.scrollTop = isScrollUp ? currentY - step : currentY + step;
+    };
+
     const clickBtnDown = () => {
-        // if (!chat?.pending_messages_count) {
         executeScrollToElement({ ref: bottomMessageRef, enable: true });
-        // } else {
-        //     executeScrollToElement({ ref: firstUnreadMessageRef, enable: true });
-        // }
     };
 
     useEffect(() => {
@@ -134,7 +136,7 @@ function MessagesListView(props: Props) {
         if (inViewPrevPage) getPrevPage();
         if (inViewNextPage) getNextPage();
     }, [inViewPrevPage, inViewNextPage]);
-    console.log(isFetching);
+
     useEffect(() => {
         subscribeToChat(inViewLastMessageCheckVisibleRef ? 'sub' : 'unsub');
     }, [inViewLastMessageCheckVisibleRef, chat?.pending_messages_count]);
@@ -149,93 +151,62 @@ function MessagesListView(props: Props) {
             executeScrollToElement({ ref: bottomMessageRef, enable: true });
         }
     }, [goDownList]);
-    // const track: any = document.getElementsByClassName('ScrollbarsCustom-TrackY');
-    // const thumb: any = document.getElementsByClassName('ScrollbarsCustom-ThumbY');
-
-    useLifecycles(() => {
-        // if (track[0]) {
-        //     track[0].style.transition = 'opacity 0.5s';
-        //     track[0].style.backgroundColor = 'inherit';
-        //     // track[0].style.opacity = 0;
-        //     thumb[0].style.backgroundColor = 'var(--control-primary)';
-        // }
-    });
 
     return (
-        <Scrollbar
-        // scrollbarWidth={6}
-        // scrollDetectionThreshold={2000}
-        // onScroll={() => {
-        //     if (track[0]) {
-        //         track[0].style.opacity = 1;
-        //     }
-        // }}
-        // onScrollStop={() => {
-        //     if (track[0]) {
-        //         track[0].style.opacity = 0;
-        //     }
-        // }}
+        <div
+            onWheel={onWheel}
+            className={`${styles.wrapper} ${isFileDrag.value ? styles.wrapper_dragOver : ''}`}
+            ref={mergeRefs([wrapperRef, dropContainerRef])}
+            onDragOver={() => isFileDrag.set(true)}
+            onDragLeave={() => isFileDrag.set(false)}
+            onDrop={() => isFileDrag.set(false)}
         >
-            <div
-                onWheel={(e) => {
-                    const isScrollUp = e.deltaY < 0;
-                    const step = 50;
-                    const currentY = wrapperRef.current.scrollTop;
-                    wrapperRef.current.scrollTop = isScrollUp ? currentY - step : currentY + step;
-                }}
-                className={`${styles.wrapper} ${isFileDrag.value ? styles.wrapper_dragOver : ''}`}
-                ref={mergeRefs([wrapperRef, dropContainerRef])}
-                onDragOver={() => isFileDrag.set(true)}
-                onDragLeave={() => isFileDrag.set(false)}
-                onDrop={() => isFileDrag.set(false)}
-            >
-                <Box.Animated visible={!inViewLastMessageCheckVisibleRef} className={styles.btnDown}>
-                    <Counter>{chat?.pending_messages_count || 0}</Counter>
-                    <Button.Circle radius={34} onClick={clickBtnDown}>
-                        <Icons variant="arrow-drop-down" />
-                    </Button.Circle>
-                </Box.Animated>
-                <div ref={mergeRefs([bottomMessageRef, bottomMessageCheckVisibleRef])} />
-                {messages?.map((message, index) => (
-                    <Fragment key={message.id}>
-                        {message.type !== 'system' && (
-                            <div
-                                onClick={() => rowClick(message)}
-                                onMouseEnter={() => !message.is_read && readMessage(message.id)}
-                                className={`${styles.row} ${
-                                    highlightedMessages.value.find((i) => i.id === message.id) || message.id === foundMessage?.id ? styles.row_active : ''
-                                }`}
-                                style={{
-                                    justifyContent: message.isMy ? 'flex-end' : 'flex-start',
-                                    cursor: highlightedMessages.value.length ? 'pointer' : 'default',
-                                }}
-                                ref={getMessageRefs(message, index)}
-                            >
-                                {index === 10 && <div ref={prevPageRef} />}
-                                <Message
-                                    clickMessageReply={clickMessageReply}
-                                    openChatProfileModal={openChatProfileModal}
-                                    sendReaction={sendReaction}
-                                    chat={chat}
-                                    ref={wrapperRef}
-                                    message={message}
-                                    messageMenuAction={messageMenuAction}
-                                    voiceRecordingInProgress={voiceRecordingInProgress}
-                                />
-                                {messages?.length - 10 === index && <div ref={nextPageRef} />}
-                            </div>
-                        )}
-                        {message.systemMessages.length
-                            ? message.systemMessages.map((text, ind) => (
-                                  <div key={ind} ref={getMessageRefs(message, index)} onMouseEnter={() => !message.is_read && readMessage(message.id)}>
-                                      <SystemMessage text={text} />
-                                  </div>
-                              ))
-                            : null}
-                    </Fragment>
-                ))}
-            </div>
-        </Scrollbar>
+            <Box.Animated visible={!inViewLastMessageCheckVisibleRef} className={styles.btnDown}>
+                <Counter>{chat?.pending_messages_count || 0}</Counter>
+                <Button.Circle radius={34} onClick={clickBtnDown}>
+                    <Icons variant="arrow-drop-down" />
+                </Button.Circle>
+            </Box.Animated>
+            <div ref={mergeRefs([bottomMessageRef, bottomMessageCheckVisibleRef])} />
+            {messages?.map((message, index) => (
+                <Fragment key={message.id}>
+                    {message.type !== 'system' && (
+                        <div
+                            onClick={() => rowClick(message)}
+                            onMouseEnter={() => !message.is_read && readMessage(message.id)}
+                            className={`${styles.row} ${
+                                highlightedMessages.value.find((i) => i.id === message.id) || message.id === foundMessage?.id ? styles.row_active : ''
+                            }`}
+                            style={{
+                                justifyContent: message.isMy ? 'flex-end' : 'flex-start',
+                                cursor: highlightedMessages.value.length ? 'pointer' : 'default',
+                            }}
+                            ref={getMessageRefs(message, index)}
+                        >
+                            {index === 10 && <div ref={prevPageRef} />}
+                            <Message
+                                clickMessageReply={clickMessageReply}
+                                openChatProfileModal={openChatProfileModal}
+                                sendReaction={sendReaction}
+                                chat={chat}
+                                ref={wrapperRef}
+                                message={message}
+                                messageMenuAction={messageMenuAction}
+                                voiceRecordingInProgress={voiceRecordingInProgress}
+                            />
+                            {messages?.length - 10 === index && <div ref={nextPageRef} />}
+                        </div>
+                    )}
+                    {message.systemMessages.length
+                        ? message.systemMessages.map((text, ind) => (
+                              <div key={ind} ref={getMessageRefs(message, index)} onMouseEnter={() => !message.is_read && readMessage(message.id)}>
+                                  <SystemMessage text={text} />
+                              </div>
+                          ))
+                        : null}
+                </Fragment>
+            ))}
+        </div>
     );
 }
 
