35d4442d210c25271e81f75576bfbad382e7712e
Egor <egorraybul@gmail.com>
2024-01-25 04:15:35 +0300

    upd

--- src/shared/hooks/useDivScroll.tsx
+++ src/shared/hooks/useDivScroll.tsx
@@ -2,23 +2,30 @@ import { JitsiMeeting } from '@jitsi/react-sdk';
 import { WebviewWindow } from '@tauri-apps/api/window';
 import React, { RefObject, useEffect, useRef, WheelEvent } from 'react';
 
-export type ScrollbarProps = {
-    wrapperRef: RefObject<HTMLDivElement>;
-};
-function useDivScroll(props: ScrollbarProps) {
-    const { wrapperRef } = props;
+import useEasyState from './useEasyState';
 
+function useDivScroll() {
     const thumbRef = useRef<HTMLDivElement>(null);
+    const thumbHeight = useEasyState(0);
+    const thumbY = useEasyState(0);
 
     const onWheel = (e: WheelEvent<HTMLDivElement>) => {
-        if (wrapperRef?.current) {
+        const wrapper = e.currentTarget;
+        if (wrapper) {
             const isScrollUp = e.deltaY < 0;
             const step = 50;
-            const currentY = wrapperRef.current.scrollTop;
-            wrapperRef.current.scrollTop = isScrollUp ? currentY - step : currentY + step;
+            const currentY = wrapper.scrollTop;
+            wrapper.scrollTop = isScrollUp ? currentY - step : currentY + step;
 
             if (thumbRef.current) {
-                const { scrollTop, scrollHeight } = wrapperRef.current;
+                const { scrollTop, scrollHeight, clientHeight } = wrapper;
+                const { width } = wrapper.getBoundingClientRect();
+                const viewHeightPercent = Math.ceil((width * 100) / scrollHeight);
+                const viewYPercent = Math.ceil((scrollTop / (scrollHeight - clientHeight)) * 100);
+                thumbY.set(-viewYPercent);
+                if (thumbHeight.value !== viewHeightPercent) {
+                    thumbHeight.set(viewHeightPercent);
+                }
             }
         }
     };
@@ -32,16 +39,18 @@ function useDivScroll(props: ScrollbarProps) {
                     right: 0,
                     height: '100%',
                     width: 12,
-                    backgroundColor: 'var(--control-tertiary)',
+                    // backgroundColor: 'var(--control-tertiary)',
                 }}
             >
                 <div
                     ref={thumbRef}
                     style={{
                         width: '100%',
-                        minHeight: 50,
+                        height: `${thumbHeight.value}%`,
                         backgroundColor: 'var(--control-primary)',
                         borderRadius: 22,
+                        position: 'absolute',
+                        bottom: `${thumbY.value}%`,
                     }}
                 />
             </div>
