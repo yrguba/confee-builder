0ff7814eabd2b88b84e509318baf2dd4c716be60
Egor <egorraybul@gmail.com>
2024-02-06 12:39:20 +0300

    upd

--- src/shared/ui/media-content/audio/ui/base/index.tsx
+++ src/shared/ui/media-content/audio/ui/base/index.tsx
@@ -17,7 +17,6 @@ function AudioBase(props: BaseAudioProps) {
     const visibleMenu = useEasyState(false);
     const notification = Notification.use();
     const visibleTiming = useEasyState(false);
-    const duration = useEasyState(10);
 
     const { src } = useFetchMediaContent({ url, name, fileType: 'audio' });
 
@@ -32,11 +31,6 @@ function AudioBase(props: BaseAudioProps) {
     useAudioStore.use.observer();
     const togglePlay = useAudioStore.use.togglePlay();
     const checkIsPlaying = useAudioStore.use.checkIsPlaying();
-
-    const [audio, state, controls, ref] = useAudio({
-        src,
-        autoPlay: false,
-    });
 
     const fs = useFs();
 
@@ -72,39 +66,15 @@ function AudioBase(props: BaseAudioProps) {
         }
     }, [progress.value]);
 
-    const totalTime = timeConverter(Math.ceil(duration.value));
-    const currentTime = timeConverter(Math.ceil(state.time));
+    const totalTime = timeConverter(Math.ceil(0));
+    const currentTime = timeConverter(Math.ceil(0));
 
     const onPlay = () => {
         togglePlay(audioRef);
     };
 
-    useEffect(() => {
-        if (String(state.duration) !== 'Infinity' && state.duration > 0) {
-            duration.set(state.duration);
-        }
-    }, [state.duration]);
-
-    useEffect(() => {
-        if (state.time > 0) {
-            visibleTiming.set(true);
-        }
-        if (state.duration === state.time) {
-            visibleTiming.set(false);
-        }
-    }, [state.time]);
-
-    // useUpdateEffect(() => {
-    //     if (audioIdCurrentlyPlaying.value !== id) {
-    //         visibleTiming.set(false);
-    //         controls.pause();
-    //         controls.seek(0);
-    //     }
-    // }, [audioIdCurrentlyPlaying.value]);
-    console.log(checkIsPlaying(src));
     return (
         <div className={styles.wrapper} onMouseLeave={() => visibleMenu.set(false)} onContextMenu={clickContextMenu}>
-            {audio}
             <audio ref={audioRef} autoPlay />
             <div className={styles.icon} onClick={onPlay}>
                 <Icons.Player variant={checkIsPlaying(src) ? 'pause' : 'play'} />
