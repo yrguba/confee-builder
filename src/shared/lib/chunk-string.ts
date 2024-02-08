829f05ca409e77f5fa2ef3761bad973c16f8c19a
Egor <egorraybul@gmail.com>
2024-02-08 23:12:37 +0300

    upd

--- src/shared/lib/chunk-string.ts
+++ src/shared/lib/chunk-string.ts
@@ -1,5 +1,12 @@
 function chunkString(str: string, length: number): string[] {
-    return str.match(new RegExp(`.{1,${length}}`, 'g')) as any;
+    const numChunks = Math.ceil(str.length / length);
+    const chunks = new Array(numChunks);
+
+    for (let i = 0, o = 0; i < numChunks; ++i, o += length) {
+        chunks[i] = str.substr(o, length);
+    }
+
+    return chunks;
 }
 
 export default chunkString;
