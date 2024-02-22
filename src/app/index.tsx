166b5ea85c53d6a0e6b3ec016032f72f9bf73138
Egor <egorraybul@gmail.com>
2024-02-22 19:30:56 +0300

    upd

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -21,6 +21,7 @@ const queryClient = new QueryClient({
             refetchOnReconnect: true,
             retry: 0,
             cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+            refetchOnWindowFocus: false,
         },
     },
 });
