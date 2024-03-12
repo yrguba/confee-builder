d4d3ce9aa8c13cf6ed4ac8ed895fea1c6d3676b0
Egor <egorraybul@gmail.com>
2024-03-12 14:24:18 +0300

    upd

--- src/pages/update-app/ui/index.tsx
+++ src/pages/update-app/ui/index.tsx
@@ -21,22 +21,22 @@ function UpdateAppPage() {
                 <img src={confeeLogo} alt="" />
             </div>
             <div className={styles.modal}>
+                <Title textAlign="center" variant="H3S">
+                    Доступно обновление
+                </Title>
                 <div className={styles.titles}>
-                    <Title textAlign="center" variant="H1">
-                        Доступно обновление
+                    <Title textAlign="center" textWrap primary={false} variant="H4R">
+                        Чтобы воспользоваться всеми возможностями приложения, обновите его до последней версии
                     </Title>
-                    <Title textAlign="center" textWrap primary={false} variant="H3S">
-                        Чтобы воспользоваться всеми возможностями приложения, обновите его до последней версии.
-                    </Title>
-                    <Title textAlign="center" textWrap variant="H3S">
-                        Если вы используете портативную версию - то перезапустите приложение.
+                    <Title textAlign="center" textWrap primary={false} variant="H4R">
+                        Если вы используете портативную версию, то перезапустите приложение
                     </Title>
                 </div>
                 <div className={styles.btns}>
-                    <Button variant="secondary" width="120px" height="40px" onClick={() => navigate(-1)}>
+                    <Button variant="secondary" height="40px" onClick={() => navigate(-1)}>
                         Пропустить
                     </Button>
-                    <Button width="120px" height="40px" onClick={updateApp}>
+                    <Button height="40px" onClick={updateApp}>
                         Обновить
                     </Button>
                 </div>
